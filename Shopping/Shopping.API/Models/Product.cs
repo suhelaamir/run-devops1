7bcee042263d04df0e82d4fb79f9aeb899a67569
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-08-06 16:41:27 +0530

    fix nullbale issue

--- Shopping/Shopping.API/Models/Product.cs
+++ Shopping/Shopping.API/Models/Product.cs
@@ -6,7 +6,7 @@ public class Product
 {
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
