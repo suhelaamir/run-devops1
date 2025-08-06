a090d4b59a3431d442515b28950244f42889521f
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-08-06 22:28:33 +0530

    tt fixes

--- Shopping/Shopping.API/Models/Product.cs
+++ Shopping/Shopping.API/Models/Product.cs
@@ -12,4 +12,5 @@ public class Product
     public string Description { get; set; } = string.Empty;
     public string ImageFile { get; set; } = string.Empty;
     public decimal Price { get; set; }
+
 }
