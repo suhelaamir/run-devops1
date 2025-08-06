ec09cb78525d45dfe1a0862b5d88423622efe6ee
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-08-06 17:35:35 +0530

    issue fixes

--- Shopping/Shopping.API/Models/Product.cs
+++ Shopping/Shopping.API/Models/Product.cs
@@ -11,6 +11,5 @@ public class Product
     public string Category { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageFile { get; set; } = string.Empty;
-    public decimal Price { get; set; } 
-
+    public decimal Price { get; set; }
 }
